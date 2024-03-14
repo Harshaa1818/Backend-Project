1ffbfc016767db34bd90595c55ffc60728a3a707
Harshaa1818 <114155862+Harshaa1818@users.noreply.github.com>
2024-03-14 13:31:00 +0530

    api_response

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors"
 
 const app=express()
 
+app.use(cors(
+{
+    origin: process.env.ORIGIN_NAME,
+    credentials: true
+
+}))
+
+app.use(express.json({limit: "16 Kb"}))
+app.use(express.urlencoded({limit: "16kb", extended:true}))
+app.use(express.static())
 export default app;
